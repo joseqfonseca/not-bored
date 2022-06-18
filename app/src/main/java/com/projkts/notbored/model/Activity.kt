6a11ebf5038eba800e3e0473274db7c8d22d3fcd
joseqfonseca <jose.qfonseca@mercadolivre.com>
2022-06-18 11:00:31 -0300

    Initial Commit

---
+++ app/src/main/java/com/projkts/notbored/model/Activity.kt
@@ -0,0 +1,8 @@
+package com.projkts.notbored.model
+
+data class Activity(
+    val title: String,
+    val participants: Int,
+    val price: Float,
+    val category: Category
+)
---
+++ app/src/main/java/com/projkts/notbored/model/Category.kt
@@ -0,0 +1,13 @@
+package com.projkts.notbored.model
+
+enum class Category() {
+    EDUCATION,
+    RECREATIONAL,
+    SOCIAL,
+    DIY,
+    CHARITY,
+    COOKING,
+    RELAXATION,
+    MUSIC,
+    BUSYWORK
+}
