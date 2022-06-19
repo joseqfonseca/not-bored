39a32937fa4ba2b826a25ba0bd59c1c68e2416f3
joseqfonseca <jose.qfonseca@mercadolivre.com>
2022-06-19 13:34:53 -0300

    FEATURE: Splash screen

---
+++ app/src/main/java/com/projkts/notbored/view/splash/SplashActivity.kt
@@ -0,0 +1,33 @@
+package com.projkts.notbored.view.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import com.projkts.notbored.MainActivity
+import com.projkts.notbored.R
+import kotlin.concurrent.thread
+import kotlin.coroutines.coroutineContext
+
+
+class SplashActivity : AppCompatActivity(R.layout.activity_splash) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_splash)
+
+        //showSlash()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        showSlash()
+    }
+
+    private fun showSlash() {
+        thread(true) {
+            Thread.sleep(2000L)
+            startActivity(Intent("", null, this, MainActivity::class.java))
+            finish()
+        }
+    }
+}
