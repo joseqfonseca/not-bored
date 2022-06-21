ae25a7aae5a2a462983208fdcf82e10061170f9e
joseqfonseca <jose.qfonseca@mercadolivre.com>
2022-06-21 13:06:01 -0300

    UPDATE

--- app/src/main/java/com/projkts/notbored/view/term/TermFragment.kt
+++ app/src/main/java/com/projkts/notbored/view/term/TermFragment.kt
@@ -6,55 +6,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.projkts.notbored.R
+import com.projkts.notbored.databinding.FragmentTermBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [TermFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class TermFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class TermFragment : Fragment(R.layout.fragment_term) {
+
+    private val binding: FragmentTermBinding by lazy {
+        FragmentTermBinding.inflate(layoutInflater)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_term, container, false)
+        setBtnCloseTermsListener()
+        return binding.root//inflater.inflate(R.layout.fragment_term, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TermFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TermFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun setBtnCloseTermsListener() {
+        binding.btnCloseTerm.setOnClickListener {
+            //close the fragment
+        }
     }
+
 }
